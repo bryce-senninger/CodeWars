1bcee0d3c9757dc7eaac77b1b7d9b7327ac33cee
Bryce <bryce.senninger@gmail.com>
2020-02-20 13:54:31 -0700

    new completed challenges

---
+++ 7kyu/balancednumber.js
@@ -0,0 +1,82 @@
+//Balanced Number (Special Number Series #1)
+
+// Definition
+
+// Balanced number is the number that * The sum of all digits to the left of the middle digit(s) and the sum of all digits to the right of the middle digit(s) are equal*.
+// Task
+
+// Given a number, Find if it is Balanced or not .
+// Warm-up (Highly recommended)
+// Playing With Numbers Series
+// Notes
+
+//     If the number has an odd number of digits then there is only one middle digit, e.g. 92645 has middle digit 6; otherwise, there are two middle digits , e.g. 1301 has middle digits 3 and 0
+
+//     The middle digit(s) should not be considered when determining whether a number is balanced or not, e.g 413023 is a balanced number because the left sum and right sum are both 5.
+
+//     Number passed is always Positive .
+
+//     Return the result as String
+
+// Input >> Output Examples
+
+// (balanced-num 7) ==> return "Balanced"
+
+// Explanation:
+
+//     Since , The sum of all digits to the left of the middle digit (0)
+
+//     and the sum of all digits to the right of the middle digit (0) are equal , then It's Balanced
+
+// (balanced-num 295591) ==> return "Not Balanced"
+
+// Explanation:
+
+//     Since , The sum of all digits to the left of the middle digits (11)
+
+//     and the sum of all digits to the right of the middle digits (10) are Not equal , then It's Not Balanced
+
+//     Note : The middle digit(s) are 55 .
+
+// (balanced-num 959) ==> return "Balanced"
+
+// Explanation:
+
+//     Since , The sum of all digits to the left of the middle digits (9)
+
+//     and the sum of all digits to the right of the middle digits (9) are equal , then It's Balanced
+
+//     Note : The middle digit is 5 .
+
+// (balanced-num 27102983) ==> return "Not Balanced"
+
+// Explanation:
+
+//     Since , The sum of all digits to the left of the middle digits (10)
+
+//     and the sum of all digits to the right of the middle digits (20) are Not equal , then It's Not Balanced
+
+//     Note : The middle digit(s) are 02 .
+
+function balancedNum(number) {
+  let str = number.toString();
+  let middleIndex = [];
+  let firstHalf = [];
+  let secondHalf = [];
+  if (str.length <= 2) {
+    return "Balanced";
+  } else if (str.length % 2 == 0) {
+    middleIndex.push(str.length / 2 - 1);
+    middleIndex.push(str.length / 2);
+    firstHalf = str.split("").splice(0, middleIndex[0]);
+    secondHalf = str.split("").splice(middleIndex[1] + 1, str.length);
+  } else {
+    middleIndex.push(Math.floor(str.length / 2));
+    firstHalf = str.split("").splice(0, middleIndex[0]);
+    secondHalf = str.split("").splice(middleIndex[0] + 1, str.length);
+  }
+  return firstHalf.map(Number).reduce((a, b) => a + b) ==
+    secondHalf.map(Number).reduce((a, b) => a + b)
+    ? "Balanced"
+    : "Not Balanced";
+}
