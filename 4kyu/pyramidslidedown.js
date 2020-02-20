f858f7266644bbaec570cc81e933d5e53e4569f0
Bryce <bryce.senninger@gmail.com>
2020-02-19 21:26:10 -0700

    new completed challenges

--- 4kyu/pyramidslidedown.js
+++ 4kyu/pyramidslidedown.js
@@ -32,6 +32,16 @@ function longestSlideDown(pyramid) {
   return pyramid[0][0];
 }
 
+//cleaner
+
+const longestSlideDown = pyramid => {
+  return pyramid.reduceRight((prev, curr) =>
+    curr.map((val, i) => val + Math.max(prev[i], prev[i + 1]))
+  )[0];
+};
+
+//First try, works on some cases. Doesn't account for higher numbers at the bottom missed because of top down prioritization.
+
 // function longestSlideDown(pyramid) {
 //   let tempIndex = 0;
 //   let nums = [];
