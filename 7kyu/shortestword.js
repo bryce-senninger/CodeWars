80ea5c09a5e5717105110e2e6294caaaf08a4935
Bryce <bryce.senninger@gmail.com>
2020-03-01 23:57:50 -0700

    new completed challenges

---
+++ 7kyu/shortestword.js
@@ -0,0 +1,16 @@
+//Shortest Word
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s) {
+  let words = s.split(" ");
+  let min = words[0].length;
+  for (i = 1; i < words.length; i++) {
+    if (words[i].length < min) {
+      min = words[i].length;
+    }
+  }
+  return min;
+}
